d99d2406089746fd01bd7b991f3333e3afe0fe75
jonathan <jonathan@pix8.co.uk>
2023-08-07 15:24:49 +0100

    jest swapped for vitest; amazingly painless. api appears identical

--- src/js/algorithm.test.js
+++ src/js/algorithm.test.js
@@ -1,15 +1,16 @@
+import { expect, test } from 'vitest'
 import { example1, example2, example3 } from './examples'
 import { mapStringToArray } from './utils'
 import getFastestSlideDown from './algorithm'
 
-test("Solution to example 1", () => {
+test('Solution to example 1', () => {
   expect(getFastestSlideDown(mapStringToArray(example1))).toBe(14)
 })
 
-test("Solution to example 2", () => {
+test('Solution to example 2', () => {
   expect(getFastestSlideDown(mapStringToArray(example2))).toBe(16)
 })
 
-test("Solution to example 3", () => {
+test('Solution to example 3', () => {
   expect(getFastestSlideDown(mapStringToArray(example3))).toBe(447)
 })
